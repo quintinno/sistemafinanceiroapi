9dff750fb10691fd49ada99f34fad9fb80dbf7a4
Dysprosium <dysprosium@outlook.com.br>
2023-02-17 10:08:16 -0300

    refactor: Refatoração das Configurações de Segurança

--- src/main/java/br/com/quintinno/sistemafinanceiroapi/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/quintinno/sistemafinanceiroapi/configuration/SecurityConfiguration.java
@@ -1,16 +1,16 @@
 package br.com.quintinno.sistemafinanceiroapi.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
-@SuppressWarnings("deprecation")
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 	
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.httpBasic().and().authorizeHttpRequests().anyRequest().authenticated().and().csrf().disable();
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity.httpBasic().and().authorizeHttpRequests().anyRequest().authenticated().and().csrf().disable().build();
 	}
 
 }
